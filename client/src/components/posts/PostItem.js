6fad28c804f99b962673ee29bf607c1712f9d187
Jabulani Setaka <jsetaka@meditech.co.za>
2018-05-31 16:13:53 +0200

    make it better

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -36,7 +36,7 @@ class PostItem extends Component {
           <div className="col-md-2">
             <a href="profile.html">
               <img
-                className="rounded-circle d-none d-md-block"
+                className="rounded-circle"
                 src={post.avatar}
                 alt=""
               />
@@ -44,44 +44,34 @@ class PostItem extends Component {
             <br />
             <p className="text-center">{post.name}</p>
           </div>
-          <div className="col-md-10">
-            <p className="lead">{post.text}</p>
-            {showActions ? (
-              <span>
-                <button
-                  onClick={this.onLikeClick.bind(this, post._id)}
-                  type="button"
-                  className="btn btn-light mr-1"
-                >
-                  <i
-                    className={classnames('fas fa-thumbs-up', {
+         <div className="col-md-10">
+                <p className="lead text-muted">
+                {post.name}
+                </p>
+                <p>
+                  {post.text}
+                </p>
+                  {showActions ? (<span>
+                  <button onClick={this.onLikeClick.bind(this, post._id)} type="button" className="btn btn-light mr-1">
+                    <i className={classnames('fas fa-thumbs-up', {
                       'text-info': this.findUserLike(post.likes)
-                    })}
-                  />
-                  <span className="badge badge-light">{post.likes.length}</span>
-                </button>
-                <button
-                  onClick={this.onUnlikeClick.bind(this, post._id)}
-                  type="button"
-                  className="btn btn-light mr-1"
-                >
-                  <i className="text-secondary fas fa-thumbs-down" />
-                </button>
-                <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
-                  Comments
-                </Link>
-                {post.user === auth.user.id ? (
-                  <button
-                    onClick={this.onDeleteClick.bind(this, post._id)}
-                    type="button"
-                    className="btn btn-danger mr-1"
-                  >
+                    })}></i>
+                    <span className="badge badge-light">{post.likes.length}</span>
+                  </button>
+                  <button  onClick={this.onUnlikeClick.bind(this, post._id)} type="button" className="btn btn-light mr-1">
+                    <i className="text-secondary fas fa-thumbs-down"></i>
+                  </button>
+                  <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+                    Comments
+                  </Link>
+                  {post.user ===  auth.user.id ? (
+                  <button onClick={this.onDeleteClick.bind(this, post._id)} type="button" className="btn btn-danger mr-1">
                     <i className="fas fa-times" />
                   </button>
-                ) : null}
-              </span>
-            ) : null}
-          </div>
+                  ) : null}
+
+                  </span>) : null}
+                </div>
         </div>
       </div>
     );
