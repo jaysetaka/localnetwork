f3a24c6b918c5e1c3ec3457422a2db544dd6a6de
Jabulani Setaka <jsetaka@meditech.co.za>
2018-05-31 18:11:21 +0200

    make it better

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -25,7 +25,7 @@ class Landing extends Component {
                   Seekers - Search for the service you need through clicking on the providers profile link bar and browse through their profiles
                 </p>
                 <br/>
-                <em className="d-none d-md-block">Pleas Note that this application is still under production, much changes are going to happen, functionalities like direct messages, followers, media upload, the user interface etc are still yet to be implimnted, they are already built, but they still need to be included in the project files</em> <strong>-Jabulani Setaka(Founder and Creator)</strong>
+                <em className="d-none d-md-block">Pleas Note that this application is still under production, much changes are going to happen, functionalities like direct messages, followers, media upload, the user interface etc are still yet to be implimnted, they are already built, but they still need to be included in the project files</em> 
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
