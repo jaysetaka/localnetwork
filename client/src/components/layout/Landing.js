bf954f5070b614c97fc8275e5b7877f245e9000c
Jabulani Setaka <jsetaka@meditech.co.za>
2018-06-01 10:23:47 +0200

    prepare to upload

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,7 +16,8 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Local Network Hub</h1>
+                <h1 className="display-3 mb-3">LNH</h1>
+                <h6 className="display-1 mb-1">Local Net Hub</h6>
                 <hr/>
                  <button type="button" className="btn btn-dark btn-lg mb-5" id="modalView" data-toggle="modal" data-target="#exampleModal">
                   About
